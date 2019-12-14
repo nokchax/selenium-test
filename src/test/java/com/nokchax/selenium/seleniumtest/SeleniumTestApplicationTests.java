20d20c2a120a1b7887ac1ca2b7770ee339d6e366
nokchax <nokchax@gmail.com>
2019-12-14 22:59:39 +0900

    init project

---
+++ src/test/java/com/nokchax/selenium/seleniumtest/SeleniumTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nokchax.selenium.seleniumtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeleniumTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
