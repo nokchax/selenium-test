5bb5621d539063c89f01a3147c5acd6dcb22d33e
nokchax <nokchax@gmail.com>
2019-12-14 23:27:44 +0900

    todo : run this app using escape api

--- src/main/java/com/nokchax/selenium/seleniumtest/SeleniumTestApplication.java
+++ src/main/java/com/nokchax/selenium/seleniumtest/SeleniumTestApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 @SpringBootApplication
 public class SeleniumTestApplication {
 
+    // TODO: 2019/12/14 run this app using below
+    // https://www.baeldung.com/run-shell-command-in-java
     public static void main(String[] args) {
         new SpringApplicationBuilder().web(WebApplicationType.NONE)
                 .sources(SeleniumTestApplication.class)
---
+++ src/main/java/com/nokchax/selenium/seleniumtest/component/Selenium.java
@@ -0,0 +1,13 @@
+package com.nokchax.selenium.seleniumtest.component;
+
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+public class Selenium {
+    @PostConstruct
+    public void init() {
+        System.out.println("test");
+    }
+}
