6d676044d29930730f9612c3a4f871f39c4684ef
nokchax <nokchax@gmail.com>
2019-12-14 23:36:41 +0900

    comment : add link

--- src/main/java/com/nokchax/selenium/seleniumtest/SeleniumTestApplication.java
+++ src/main/java/com/nokchax/selenium/seleniumtest/SeleniumTestApplication.java
@@ -9,6 +9,7 @@ public class SeleniumTestApplication {
 
     // TODO: 2019/12/14 run this app using below
     // https://www.baeldung.com/run-shell-command-in-java
+    // https://www.mkyong.com/java/how-to-execute-shell-command-from-java/
     public static void main(String[] args) {
         new SpringApplicationBuilder().web(WebApplicationType.NONE)
                 .sources(SeleniumTestApplication.class)
