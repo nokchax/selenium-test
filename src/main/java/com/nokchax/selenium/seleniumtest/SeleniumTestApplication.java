fa29d905b8281df639e341a44fce4678ba68f251
nokchax <nokchax@gmail.com>
2019-12-19 19:49:44 +0900

    refactor : change app use local web driver not driver that exist in jar file

--- src/main/java/com/nokchax/selenium/seleniumtest/SeleniumTestApplication.java
+++ src/main/java/com/nokchax/selenium/seleniumtest/SeleniumTestApplication.java
@@ -1,6 +1,5 @@
 package com.nokchax.selenium.seleniumtest;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
